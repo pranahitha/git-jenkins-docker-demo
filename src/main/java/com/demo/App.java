d8dc11edb2b6785fe6687e466e99d0bbd16e2a66
pranahitha <pranahithayadav@gmail.com>
2021-09-08 13:05:08 +0530

    Second Commit

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Pranahitha!" );
+        System.out.println( "Hello Jadala Pranahitha!" );
     }
 }
