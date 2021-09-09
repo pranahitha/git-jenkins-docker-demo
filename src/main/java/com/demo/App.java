2f9b1851994653a7b100e5bb75961943a8896c32
pranahitha <pranahithayadav@gmail.com>
2021-09-09 13:27:07 +0530

    changes commit

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -10,4 +10,12 @@ public class App
     {
         System.out.println( "Hello Jadala Pranahitha!" );
     }
+    public void message()
+    {
+    	System.out.println("Printing message");
+    }
+    public int sum(int num1,int num2)
+    {
+    	return (num1-num2);
+    }
 }
---
+++ src/test/java/com/test/AppTest.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.demo.App;
+
+public class AppTest {
+	App app;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		app=new App();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		app=null;
+	}
+
+	@Test
+	public void sumtest1() {
+		assertEquals(12, app.sum(6, 6));;
+	}
+	@Test
+	public void sumtest2() {
+		assertEquals(32, app.sum(16, 16));;
+	}
+	@Test
+	public void sumtest3() {
+		assertEquals(20, app.sum(14, 6));;
+	}
+
+}
